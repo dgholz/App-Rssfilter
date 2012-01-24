f1fddd1cd69b8d734397d0110dc192d54184896b
Daniel Holz <dgholz@gmail.com>
2012-01-25 09:13:25 +1100

    more admin, activating perlbrew in the cron job ..

--- cron.sh
+++ cron.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+source ~/perl5/perlbrew/etc/bashrc
+
 RSS_DEDUPE_DIR=`dirname "$( readlink -f "$BASH_SOURCE" )"`
-$RSS_DEDUPE_DIR/strip_sports.pl >> $RSS_DEDUPE_DIR/log 2>&1
+mkdir -p "$RSS_DEDUPE_DIR/www"
+cd "$RSS_DEDUPE_DIR/www"
+perl "$RSS_DEDUPE_DIR/strip_sports.pl" >> "$RSS_DEDUPE_DIR/log" 2>&1
