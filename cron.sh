eb66c29e3a14d777bed9c782ce89fbbba1b4ca97
Daniel Holz <dgholz@gmail.com>
2012-05-03 04:02:58 +1000

    updating cron job to add lib dir to lib search path

--- cron.sh
+++ cron.sh
@@ -5,4 +5,4 @@ source ~/perl5/perlbrew/etc/bashrc
 RSS_DEDUPE_DIR=`dirname "$( readlink -f "$BASH_SOURCE" )"`
 mkdir -p "$RSS_DEDUPE_DIR/www"
 cd "$RSS_DEDUPE_DIR/www"
-perl "$RSS_DEDUPE_DIR/bin/rssfilter" $@ >> "$RSS_DEDUPE_DIR/log" 2>&1
+perl -I "$RSS_DEDUPE_DIR/lib" "$RSS_DEDUPE_DIR/bin/rssfilter" $@ >> "$RSS_DEDUPE_DIR/log" 2>&1
