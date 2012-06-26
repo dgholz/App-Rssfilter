f5029c89e9d2ad4f92b363a1a796961e19431c5d
Daniel Holz <dgholz@gmail.com>
2012-06-27 00:03:58 +0100

    stopping AbcPreviews from matching items which have 'preview' in their title

--- lib/Rss/Match/AbcPreviews.pm
+++ lib/Rss/Match/AbcPreviews.pm
@@ -5,7 +5,7 @@ use feature qw( :5.14 );
 package Rss::Match::AbcPreviews {
     use Method::Signatures;
     func match ( $item ) {
-        return $item->guid->text =~ / preview /xms;
+        return $item->guid->text =~ / [^-] preview /xms;
     }
 }
 
