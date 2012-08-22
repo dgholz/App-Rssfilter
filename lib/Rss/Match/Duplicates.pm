5b37ef46c8adf26700fa30a38f4676882be8bda5
Daniel Holz <dgholz@gmail.com>
2012-08-23 00:30:22 +0100

    making Rss::Match::Duplicates more forgiving when RSS items are missing link or guid elements

--- lib/Rss/Match/Duplicates.pm
+++ lib/Rss/Match/Duplicates.pm
@@ -4,12 +4,14 @@ use feature qw( :5.14 );
 
 package Rss::Match::Duplicates {
     use Method::Signatures;
+    use Try::Tiny;
     func match ( $item ) {
         state %prev;
-        my $link = $item->guid->text;
-        my $url  = $item->link->text =~ s/ [?] .* \z//xmsr;
-        my $res  = grep { defined } @prev{ $link, $url };
-        @prev{ $link, $url } = ( 1, 1 );
+        my $link = try { $item->guid->text } || "";
+        my $url  = try { $item->link->text =~ s/ [?] .* \z//xmsr } || "";
+        my @matchables = grep { $_ ne "" } $link, $url;
+        my $res  = grep { defined } @prev{ @matchables };
+        @prev{ @matchables } = ( 1 ) x @matchables;
         return 0 < $res;
     }
 }
