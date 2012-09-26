c4b062d64cb7486072e6d639a030be9eb040132d
Daniel Holz <dgholz@gmail.com>
2012-09-26 23:40:56 +0100

    adding overview doco for App::Rssfilter

--- lib/App/Rssfilter.pm
+++ lib/App/Rssfilter.pm
@@ -1,6 +1,40 @@
-#!/usr/bin/env perl
-
 # ABSTRACT: Fetch and filter RSS feeds
+
+=head1 SYNOPSIS
+
+    use App::RssFilter;
+
+    my $rssfilter = App::RssFilter->new;
+    $rssfilter->add_group( 'hi' );
+    my $hello_group = $rssfilter->add_group( 'hello' );
+
+    my $dupe_rule = $rssfilter->group( 'hi' )->add_rule( Duplicate => 'DeleteItem' );
+    $hello_group->add_rule( condition => 'Category[Politics]', action => 'MarkTitle' );
+    $hello_group->add_rule( $dupe_rule );
+
+    $rssfilter->group( 'hi' )->add_feed( WashPost => 'http://feeds.washingtonpost.com/rss/national' );
+    $rssfilter->group( 'hi' )->add_feed( name => 'Pravda', url => 'http://english.pravda.ru/russia/export.xml' );
+
+    $hello_group->add_feed( $rssfilter->group( 'hi' )->feed( 'WashPost' ) );
+
+    $rssfilter->run;
+
+=head1 DESCRIPTION
+
+=cut
+
+
+
+=head1 SEE ALSO
+
+=for :list
+* L<App::RssFilter::Group>
+* L<App::RssFilter::Rule>
+* L<App::RssFilter::Feed>
+
+=cut
+
+
 
 use strict;
 use warnings;
