8bf4c265f5084ea8fce9a626e7d27d0b0a1e0ec3
Daniel Holz <dgholz@gmail.com>
2013-03-11 22:37:39 +0000

    adding crummy abstract to A::R::Cmd

--- lib/App/Rssfilter/Cmd.pm
+++ lib/App/Rssfilter/Cmd.pm
@@ -1,3 +1,9 @@
+# ABSTRACT: command-line application class for App::Rssfilter
+
+use strict;
+use warnings;
+use feature qw< :5.14 >;
+
 package App::Rssfilter::Cmd {
     use constant plugin_search_path => __PACKAGE__;
     use constant default_command => 'runfromconfig';
