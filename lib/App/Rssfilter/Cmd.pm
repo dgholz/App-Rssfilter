79a0f3cd5f88bde5ea9d17a05b1bc5cb8f75da3b
Daniel George Holz <dgholz@gmail.com>
2013-09-26 23:08:46 +0100

    Release 0.04 from 50956ab

---
+++ lib/App/Rssfilter/Cmd.pm
@@ -0,0 +1,40 @@
+# ABSTRACT: App::Rssfilter's App::Cmd
+
+use strict;
+use warnings;
+
+package App::Rssfilter::Cmd;
+{
+  $App::Rssfilter::Cmd::VERSION = '0.04';
+}
+
+use constant plugin_search_path => __PACKAGE__;
+use constant default_command => 'runfromconfig';
+use App::Cmd::Setup -app;
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+App::Rssfilter::Cmd - App::Rssfilter's App::Cmd
+
+=head1 VERSION
+
+version 0.04
+
+=head1 AUTHOR
+
+Daniel Holz <dgholz@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Daniel Holz.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
